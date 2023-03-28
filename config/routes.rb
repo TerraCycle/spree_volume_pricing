5347b61964104f6d01b7f5bd6859413021acec71
ekzosta <ekzosta@gmail.com>
2023-03-28 16:08:57 +0300

    Add locale param to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Spree::Core::Engine.add_routes do
-  namespace :admin do
-    resources :products do
-      resources :variants do
-        get :volume_prices, on: :member
+  scope '(:locale)', locale: /#{Spree.available_locales.join('|')}/, defaults: { locale: nil } do
+    namespace :admin do
+      resources :products do
+        resources :variants do
+          get :volume_prices, on: :member
+        end
       end
-    end
 
-    delete '/volume_prices/:id', to: 'volume_prices#destroy', as: :volume_price
-    resources :volume_price_models
+      delete '/volume_prices/:id', to: 'volume_prices#destroy', as: :volume_price
+      resources :volume_price_models
+    end
   end
 end
