97cce86fe19b72bd5e73862a367ee33e4d7364aa
akos <84581881+akosblkvcs@users.noreply.github.com>
2023-10-07 22:49:56 +0200

    Remove Volume Price Model from Configurations

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -12,11 +12,3 @@ Deface::Override.new(
   partial: 'spree/admin/variants/edit_fields'
 )
 
-Deface::Override.new(
-  virtual_path:  'spree/admin/shared/sub_menu/_configuration',
-  name:          'add_volume_price_model_admin_menu_links',
-  insert_bottom: "[data-hook='admin_configurations_sidebar_menu']",
-  text:          <<-HTML
-                  <%= configurations_sidebar_menu_item Spree.t('volume_price_models'), spree.admin_volume_price_models_path if can? :admin, Spree::VolumePriceModel %>
-                 HTML
-)
